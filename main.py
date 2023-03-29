a81497f82b87176940b839990d84f87edf8347c2
tanya598 <obrysnik.tanya@gmail.com>
2023-03-29 18:33:55 +0300

    Записуємо відфільтровані рядки в новий файл

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ with open('input.txt', 'r') as input_file:
 keyword = 'example'
 
 filtered_lines = [line for line in input_lines if keyword in line]
+
+with open('filtered.txt', 'w') as output_file:
+    output_file.writelines(filtered_lines)
